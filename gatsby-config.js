c0b6e502ac94fa1f284ca276674128aa0fd198f4
= <=>
2018-01-23 22:07:01 -0600

    changes some netlify config settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,14 @@ module.exports = {
     title: 'Gatsby Default Starter',
   },
   plugins: [
-    'gatsby-plugin-netlify-cms',
-    'gatsby-plugin-react-helmet'
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-netlify-cms`,
+      options: {
+        // One convention is to place your Netlify CMS customization code in a
+        // `src/cms` directory.
+        // modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
   ],
 };
