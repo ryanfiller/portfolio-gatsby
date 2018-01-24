a29279b26f8821c6e04bd13efc26aedde4408a1f
= <=>
2018-01-23 19:10:45 -0600

    adds scss

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'ryanfiller.com',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    `gatsby-plugin-sass`
+  ]
 };
