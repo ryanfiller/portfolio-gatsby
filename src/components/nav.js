cdf2bcd372fbf5fc8f9ae355e406ebd7041cbe7f
Ryan Filler <=>
2018-01-28 20:14:59 -0600

    styles main nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,16 +4,18 @@ import Link from 'gatsby-link'
 export default class Nav extends Component {
   render() {
     return (
-      <ul>
-        {console.log(this.props.content)}
-        {Array.from(this.props.content).map(({node}, index) =>
-            <Link className="link" 
-            key={index}
-            to={node.frontmatter.title}>
-              {node.frontmatter.title}
-            </Link>
+      <nav className="nav" role="navigation">
+        <ul className="nav__list">
+          {console.log(this.props.content)}
+          {Array.from(this.props.content).map(({node}, index) =>
+            <li className="nav__link">
+              <Link key={index} to={node.frontmatter.title}>
+                {node.frontmatter.title}
+              </Link>
+            </li>
           )}
-      </ul>
+        </ul>
+      </nav>
     )
   }
 }
