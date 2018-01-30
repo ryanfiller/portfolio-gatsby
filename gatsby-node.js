29ea8289e40558226a7432a0c74823fa7fa60d89
Ryan Filler <=>
2018-01-30 07:18:42 -0600

    sets up templates and slugs

--- gatsby-node.js
+++ gatsby-node.js
@@ -60,111 +60,15 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
           }
 
           createPage({
-          path: urlPath,
+          path: node.fields.slug,
           component: path.resolve('./src/templates/' + template + '.js'),
+          context: {
+                // Data passed to context is available in page queries as GraphQL variables.
+                slug: node.fields.slug,
+            },
           })
         })
         resolve()
       })
     })
   }
-
-// const path = require(`path`)
-// const { createFilePath } = require(`gatsby-source-filesystem`)
-
-// exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
-//   const { createNodeField } = boundActionCreators
-//   if (node.internal.type === `MarkdownRemark`) {
-//     const slug = createFilePath({ node, getNode, basePath: `pages` })
-//     createNodeField({
-//       node,
-//       name: `slug`,
-//       value: slug,
-//     })
-//   }
-// }
-
-// exports.createPages = ({ graphql, boundActionCreators }) => {
-//     const { createPage } = boundActionCreators
-//     return new Promise((resolve, reject) => {
-//       graphql(`
-//         {
-//           allMarkdownRemark {
-//             edges {
-//               node {
-//                 fields {
-//                   slug
-//                 }
-//               }
-//             }
-//           }
-//         }
-//       `
-//   ).then(result => {
-//         result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-//           if(node.fields.slug.includes('blog')){
-//             template = `./src/templates/blog-item.js`;
-//           } else if (node.fields.slug.includes('portfolio')) {
-//             template = `./src/templates/portfolio-item.js`;
-//           }
-//             createPage({
-//             path: node.fields.slug,
-//             component: path.resolve(`./src/templates/portfolio-item.js`),
-//             context: {
-//                 // Data passed to context is available in page queries as GraphQL variables.
-//                 slug: node.fields.slug,
-//             },
-//             })
-//         })
-//         resolve()
-//       })
-//     })
-//   }
-
-
-
-// const path = require(`path`)
-
-// exports.createPages = ({ graphql, boundActionCreators }) => {
-//     const { createPage } = boundActionCreators
-//     return new Promise((resolve, reject) => {
-//       graphql(`
-//         {
-//           allFile {
-//             edges {
-//               node {
-//                 sourceInstanceName
-//                 childMarkdownRemark{
-//                   frontmatter {
-//                     template
-//                     title
-//                   }
-//                 }
-//               }
-//             }
-//           }
-//         }
-//       `
-//   ).then(result => {
-//         result.data.allFile.edges.forEach(({ node }) => {
-//           if(node.childMarkdownRemark.frontmatter.template != null ){
-//             template = node.childMarkdownRemark.frontmatter.template;
-//           } else {
-//             template = "page";
-//           }
-
-//           if(node.sourceInstanceName.includes("blog")){
-//             urlPath = "blog/" + node.childMarkdownRemark.frontmatter.title.toLowerCase().replace(" ", "_");
-//           } else {
-//             urlPath = node.childMarkdownRemark.frontmatter.title.toLowerCase().replace(" ", "_");
-//           }
-
-//           createPage({
-//           path: urlPath,
-//           component: path.resolve('./src/templates/' + template + '.js'),
-//           })
-//         })
-//         resolve()
-//       })
-//     })
-//   }
