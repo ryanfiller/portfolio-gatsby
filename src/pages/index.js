fa086263859ffee2bb298030798bf11372e51c71
Ryan Filler <=>
2018-02-02 13:35:06 -0600

    portfolio grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import PortfolioGrid from '../components/portfolio-grid.js'
+import PortfolioGrid from '../components/portfolioGrid.js'
 
 const IndexPage = ({ data }) => {
 
