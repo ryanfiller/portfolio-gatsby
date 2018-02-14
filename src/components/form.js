919e9206e066ccb6b58d6b89283e84cd6a8c294c
Ryan Filler <=>
2018-02-13 19:03:56 -0600

    adds demo form

--- src/components/form.js
+++ src/components/form.js
@@ -1,21 +1,73 @@
-import React from 'react'
+import React from "react";
 
+function encode(data) {
+  return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+}
 
-const Form = () => (
-    <form name="contact" netlify>
-        <div>
-            <label>Your Name: <input type="text" name="name" /></label>   
-        </div>
-        <div>
-            <label>Your Email: <input type="email" name="email" /></label>
-        </div>
-        <div>
-            <label>Message: <textarea name="message"></textarea></label>
-        </div>
-        <div>
+export default class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value});
+  }
+
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Contact</h1>
+        <form
+          name="contact"
+          method="post"
+          action="/thanks/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={this.handleSubmit}
+        >
+          <p hidden>
+            <label>
+              Don’t fill this out: <input name="bot-field" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your name:<br />
+            <input type="text" name="name" onChange={this.handleChange}/>
+            </label>
+          </p>
+          <p>
+            <label>
+              Your email:<br />
+              <input type="email" name="email" onChange={this.handleChange}/>
+            </label>
+          </p>
+          <p>
+            <label>
+              Message:<br />
+              <textarea name="message" onChange={this.handleChange}/>
+            </label>
+          </p>
+          <p>
             <button type="submit">Send</button>
-        </div>
-    </form>
-)
-
-export default Form;
+          </p>
+        </form>
+      </div>
+    );
+  }
+}
