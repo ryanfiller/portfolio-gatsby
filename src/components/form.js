6e5fa4c837be8057137d3f8efa69d11da50dc1a8
Ryan Filler <=>
2018-02-17 10:46:40 -0600

    adds change handler to form fields

--- src/components/form.js
+++ src/components/form.js
@@ -37,8 +37,8 @@ export default class Form extends React.Component {
   render(state) {
     if(this.state.submitted != true) {
         return (
-            <div>
-              <form
+            <form
+                id="contact-form"
                 name="contact"
                 method="post"
                 action="/thanks/"
@@ -46,34 +46,32 @@ export default class Form extends React.Component {
                 data-netlify-honeypot="bot-field"
                 onSubmit={this.handleSubmit}
               >
-                <p hidden>
-                  <label>
-                    Don’t fill this out: <input name="bot-field" />
-                  </label>
-                </p>
-                <p>
-                  <label>
-                    Your name:<br />
-                  <input type="text" name="name" onChange={this.handleChange}/>
-                  </label>
-                </p>
-                <p>
-                  <label>
-                    Your email:<br />
-                    <input type="email" name="email" onChange={this.handleChange}/>
-                  </label>
-                </p>
-                <p>
-                  <label>
-                    Message:<br />
-                    <textarea name="message" onChange={this.handleChange}/>
-                  </label>
-                </p>
-                <p>
-                  <button type="submit">Send</button>
-                </p>
+
+                  <div className="form__row">
+                      <input type="text" name="name" placeholder="Name *" required onChange={this.handleChange} />
+                      <label htmlFor="name">Name</label>
+                  </div>
+
+                  <div className="form__row">
+                      <input type="email" name="_replyto" placeholder="Email *" required onChange={this.handleChange} />
+                      <label htmlFor="email">Email</label>
+                  </div>
+
+                  <div className="form__row">
+                      <input type="text" name="_subject" placeholder="Subject *" required onChange={this.handleChange} />
+                      <label htmlFor="subject">Subject</label>
+                  </div>
+                    
+                  <div className="form__row form__row--tall">
+                      <textarea type="text" name="_message" required placeholder="Message..." onChange={this.handleChange}></textarea>
+                      <label htmlFor="message">Message</label>
+                  </div>
+
+                  <div className="form__row">
+                      <input name="bot-field" hidden />
+                      <input className="button" type="submit" value="Send" />
+                  </div>
               </form>
-            </div>
           );
     } else {
         return(
@@ -84,53 +82,4 @@ export default class Form extends React.Component {
         );
     }
   }
-
-//   render(state) {
-//     if(this.state.submitted != true) {
-//         return (
-//               <form
-//                 id="contact-form"
-//                 name="contact"
-//                 method="post"
-//                 action="/thanks/"
-//                 data-netlify="true"
-//                 data-netlify-honeypot="bot-field"
-//                 onSubmit={this.handleSubmit}
-//               >
-
-//                   <div className="form__row">
-//                       <input type="text" name="name" placeholder="Name *" required />
-//                       <label htmlFor="name">Name</label>
-//                   </div>
-
-//                   <div className="form__row">
-//                       <input type="email" name="_replyto" placeholder="Email *" required />
-//                       <label htmlFor="email">Email</label>
-//                   </div>
-
-//                   <div className="form__row">
-//                       <input type="text" name="_subject" placeholder="Subject *" required />
-//                       <label htmlFor="subject">Subject</label>
-//                   </div>
-                    
-//                   <div className="form__row form__row--tall">
-//                       <textarea type="text" name="_message" required placeholder="Message..."></textarea>
-//                       <label htmlFor="message">Message</label>
-//                   </div>
-
-//                   <div className="form__row">
-//                       <input name="bot-field" hidden />
-//                       <input className="button" type="submit" value="Send" />
-//                   </div>
-//               </form>
-//           );
-//     } else {
-//         return(
-//             <div className="contact-form__message">
-//                 Message sent!
-//                 {/* <a onClick={this.setState({submitted: false})}>Send Another?</a> */}
-//             </div>
-//         );
-//     }
-//   }
 }
