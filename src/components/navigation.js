54c5be95a453fa5de8fecba03d1959bc2df35251
Ryan Filler <=>
2018-05-20 10:54:08 -0500

    refactors navigation to make more concise

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,27 +4,25 @@ import Link, { navigateTo } from 'gatsby-link';
 const pages = ['blog', 'about', 'contact']
 
 export default class Navigation extends Component {
-  render() {
-    return (
-      <nav className="nav" role="navigation">
-        <ul className="nav__list">
-          { pages.map((page) =>
-            {if(page != 'contact'){
-              return(<li className="nav__link" key={page}>
-                <Link to={page} data-text={page} activeClassName="active">
-                  {page}
-                </Link>
-              </li>)
-            } else {
-              return(<li className="nav__link" key={page}>
-                <a href="#contact-form" data-text={page} id={page} onClick={(e) => {this.props.toggleOffCanvas(e, '#contact-form')}} >
-                  {page}
-                </a>
-              </li>)
-            }}
-          )}
-        </ul>
-      </nav>
-    )
-  }
+	render() {
+		return (
+			<nav className="nav" role="navigation">
+				{ pages.map((page) =>
+					{if(page != 'contact'){
+						return(
+							<a onClick={ () => navigateTo(page)} className="nav__link" key={page} data-text={page} activeClassName="active">
+								{page}
+							</a>
+						)
+					} else {
+						return(
+							<a href="#contact-form" className="nav__link" key={page} data-text={page} id={page} onClick={(e) => {this.props.toggleOffCanvas(e, '#contact-form')}} >
+								{page}
+							</a>
+						)
+					}}
+				)}
+			</nav>
+		)
+	}
 }
