e8f0db9ff4503454b799385e486d9fa078764c36
Ryan Filler <=>
2018-09-05 06:19:48 -0500

    adds overlay

--- src/components/overlay.js
+++ src/components/overlay.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
-import { colors } from '../config/config';
+import { colors } from '../config/styles';
 
 export default class Overlay extends Component {
     render() {
         return (
-            <div className="page-overlay" onClick={this.props.toggleOffCanvas} />
+            <StyledOverlay className="page-overlay" onClick={this.props.toggleOffCanvas} />
         )
     }
 }
+
+const StyledOverlay = styled.div`
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background: ${colors.black};
+    opacity: .5;
+    z-index: 40;
+    
+    &.open {
+        opacity: 1;
+        pointer-events: auto;
+    }
+`
