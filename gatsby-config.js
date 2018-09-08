fb6100ea722dec3e1c2fd14d7099096029a5c5ed
Ryan Filler <=>
2018-09-07 22:30:58 -0500

    removes gatsby-plugin-styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,6 @@ module.exports = {
                 component: require.resolve(`./src/components/layout`)
             }
 		},
-		`gatsby-plugin-styled-components`,
 		`gatsby-plugin-sitemap`,
 		{
 			resolve: `gatsby-plugin-google-analytics`,
