1652e26d3b0a936824dac8a755de70e33421ffb8
Ryan Filler <=>
2018-09-08 20:02:35 -0500

    replaces npm package with helper slugify file

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,16 +1,63 @@
 const path = require(`path`)
-const slugify = require('slugify')
 const { createFilePath } = require(`gatsby-source-filesystem`)
+const { slugify } = require(`./src/helpers/helpers`)
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
 	const { createNodeField } = actions
 	if (node.internal.type === `MarkdownRemark`) {
-		const slug = createFilePath({ node, getNode, basePath: "content/" })
+		let slugDir;
+		let slugFile;
+		let slug;
+		let template;
+
+		if (node.fileAbsolutePath.includes('/portfolio/')) {
+			slugDir = '/portfolio/';
+
+			if (node.frontmatter.custompath) {
+				slugFile = node.frontmatter.custompath;
+			} else {
+				slugFile = slugify(node.frontmatter.title);
+			}
+
+			slug = slugDir + slugFile;
+
+			if (node.frontmatter.customtemplate) {
+				template = node.frontmatter.customtemplate;
+			} else {
+				template = 'portfolio-item';
+			}
+		}
+
+		if (node.fileAbsolutePath.includes('/blog/')) {
+			slugDir = '/blog/';
+
+			if (node.frontmatter.custompath) {
+				slugFile = node.frontmatter.custompath;
+			} else {
+				slugFile = slugify(node.frontmatter.title);
+			}
+
+			slug = slugDir + slugFile;
+			
+			if (node.frontmatter.customtemplate) {
+				template = node.frontmatter.customtemplate;
+			} else {
+				template = 'blog-item';
+			}
+		}
+
+		// const slug = createFilePath({ node, getNode, basePath: "content/" })
 		
 		createNodeField({
 			node,
 			name: `slug`,
 			value: slug,
+		})
+
+		createNodeField({
+			node,
+			name: `template`,
+			value: template,
 		})
 	}
 }
@@ -24,6 +71,10 @@ exports.createPages = ({ graphql, actions }) => {
 					edges {
 						node {
 							fileAbsolutePath
+							fields {
+								slug
+								template
+							}
 							frontmatter {
 								custompath
 								customtemplate
@@ -36,53 +87,11 @@ exports.createPages = ({ graphql, actions }) => {
 		`
 	).then(result => {
 		result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-
-			let slugDir;
-			let slugFile;
-			let slug;
-			let template;
-
-			if (node.fileAbsolutePath.includes('/portfolio/')) {
-				slugDir = '/portfolio/';
-
-				if (node.frontmatter.custompath) {
-					slugFile = node.frontmatter.custompath;
-				} else {
-					slugFile = slugify(node.frontmatter.title);
-				}
-
-				slug = slugDir + slugFile;
-
-				if (node.frontmatter.customtemplate) {
-					template = node.frontmatter.customtemplate;
-				} else {
-					template = 'portfolio-item';
-				}
-			}
-
-			if (node.fileAbsolutePath.includes('/blog/')) {
-				slugDir = '/blog/';
-
-				if (node.frontmatter.custompath) {
-					slugFile = node.frontmatter.custompath;
-				} else {
-					slugFile = slugify(node.frontmatter.title);
-				}
-
-				slug = slugDir + slugFile;
-				
-				if (node.frontmatter.customtemplate) {
-					template = node.frontmatter.customtemplate;
-				} else {
-					template = 'blog-item';
-				}
-			}
-
 			createPage({
-				path: slug,
-				component: path.resolve(`./src/templates/${template}.js`),
+				path: node.fields.slug,
+				component: path.resolve(`./src/templates/${node.fields.template}.js`),
 				context: {
-					slug: slug,
+					slug: node.fields.slug,
 				},
 			})
 		})
