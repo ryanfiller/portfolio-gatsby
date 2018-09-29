b3a85082e8b34faab7c95009f6f046fbcadfaf39
Ryan Filler <=>
2018-09-29 09:46:23 -0500

    stole form from phil hawksworth

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -1,24 +1,76 @@
-import React from 'react'
+import React from "react";
+import { navigateTo } from "gatsby-link";
 
-export default class Form extends React.Component {
+function encode(data) {
+  return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+}
 
-    render() {
-        return (
-            <form
-                // id="contact-form"
-                // className="form"
-                // tabIndex="1"
-                method="post"
-                data-netlify="true"
-                // data-netlify-recaptcha="true"
-                data-netlify-honeypot="bot-field"
-                // onSubmit={this.handleSubmit}
-            >
-                <input type="text" placeholder="test"/>
-                <button type="submit">submit</button>
-            </form>
-        )
-    }
+export default class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value});
+  }
+
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => navigateTo('/thanks/'))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Contact</h1>
+        <form
+          name="contact"
+          method="post"
+          action="/thanks/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={this.handleSubmit}
+        >
+          <p hidden>
+            <label>
+              Don’t fill this out: <input name="bot-field" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your name:<br />
+            <input type="text" name="name" onChange={this.handleChange}/>
+            </label>
+          </p>
+          <p>
+            <label>
+              Your email:<br />
+              <input type="email" name="email" onChange={this.handleChange}/>
+            </label>
+          </p>
+          <p>
+            <label>
+              Message:<br />
+              <textarea name="message" onChange={this.handleChange}/>
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+      </div>
+    );
+  }
 }
 
 // import React from "react";
