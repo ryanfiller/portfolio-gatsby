2a3afc7e2a3048444a1f5028dd40f61abf8c8113
Ryan Filler <ryanfiller89@gmail.com>
2019-07-04 08:26:11 -0500

    tests

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 
-import styled, { ThemeContext } from 'styled-components'
+import styled from 'styled-components'
 import MediaQuery from 'react-responsive'
 import { containers, navBreak, padding } from '../../config/styles'
 
@@ -11,37 +11,20 @@ import Navigation from './navigation'
 import Navicon from './navicon'
 
 const Header = (props) => {
-
-	const theme = useContext(ThemeContext)
-	const color = theme.darkNav ? theme.light : theme.dark;
-	const background = theme.darkNav ? theme.dark : theme.light;
-	const active = theme.active;
-
 	const nav = useContext(NavContext)
+
+	console.log('nav', nav)
 
 	return (
 		<header className={props.className} id="header">
-			<Logo
-				breakpoint={nav.currentPage === '/' ? 'desktop' : 'phone'}
-				color={color} 
-				active={active}
-				background={background}
-			/>
+			<Logo breakpoint={nav.currentPage === '/' ? 'desktop' : 'phone'}/>
 			
 			<MediaQuery query={`(min-width: ${navBreak}px)`}>
-				<Navigation 
-					color={color} 
-					active={active}
-					background={background}
-					orientation={'horizontal'}
-				/>
+				<Navigation orientation={'horizontal'} />
 			</MediaQuery>
 
 			<MediaQuery query={`(max-width: ${navBreak - 1}px)`}>
-				<Navicon 
-						color={color}
-						active={active}
-				/>
+				<Navicon />
 			</MediaQuery>
 		</header>
 	)
